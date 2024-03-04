b624f8741365e27a40b5b6907530a737754e2db9
Huijun Son <h.json248@gmail.com>
2024-03-04 12:21:47 +0900

    init commit

---
+++ src/main/java/com/hjson/chat_gpt_test/ChatGptTestApplication.java
@@ -0,0 +1,11 @@
+package com.hjson.chat_gpt_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hjson/chat_gpt_test/ChatGptTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjson.chat_gpt_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
